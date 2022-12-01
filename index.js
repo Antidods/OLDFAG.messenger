041937a1529d5be5d9404a055306d6f7780a81b5
Andre Streltsov <andrey_34rus@mail.ru>
2022-12-01 22:43:53 +0300

    added Utils

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const WINDOW = document.getElementById('main-window');
-const buttonFullScreen = document.getElementById(
-  'main-window-maximize',
-);
-const buttonClose = document.getElementById(
-  'main-window-close',
-);
 
-buttonFullScreen.addEventListener('click', () => {
-  WINDOW.classList.toggle('full-screen');
-});
 
-buttonClose.addEventListener('click', () => {
-  WINDOW.classList.add('hidden');
-});
+
+
+// const WINDOW = document.getElementById('main-window');
+// const buttonFullScreen = document.getElementById(
+//   'main-window-maximize',
+// );
+// const buttonClose = document.getElementById(
+//   'main-window-close',
+// );
+//
+// buttonFullScreen.addEventListener('click', () => {
+//   WINDOW.classList.toggle('full-screen');
+// });
+//
+// buttonClose.addEventListener('click', () => {
+//   WINDOW.classList.add('hidden');
+// });
---
+++ src/utils/EventBus.ts
@@ -0,0 +1,39 @@
+class EventBus {
+    private readonly listeners: Record<string, Array<(...args:unknown[]) => void>> = {};
+
+    constructor() {
+        this.listeners = {};
+    }
+
+    on(event: string, callback: () => void) {
+        if (!this.listeners[event]) {
+            this.listeners[event] = [];
+        }
+
+        this.listeners[event].push(callback);
+    }
+
+    off(event: string, callback: () => void) {
+        if (!this.listeners[event]) {
+            throw new Error(`Нет события: ${event}`);
+            return;
+        }
+
+        this.listeners[event] = this.listeners[event].filter(
+            listener => listener !== callback
+        );
+    }
+
+    emit(event: string, ...args: unknown[]): void {
+        if (!this.listeners[event]) {
+            throw new Error(`Нет события: ${event}`);
+            return;
+        }
+
+        this.listeners[event].forEach(listener => {
+            listener(...args);
+        });
+    }
+}
+
+export default EventBus;
